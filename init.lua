b081e9e612ed92fe3c31b16fa8262e1802554638
Ara <63669353+gamedev-Ara@users.noreply.github.com>
2022-05-29 07:50:14 +0430

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+--app name
+appname="AraModLoader"
+--app version
+appver="1.0"
+--app code
+appcode="1"
+--SDK
+appsdk="15"
+--package name
+packagename="com.aramod.loader.codm"
+--debug mode
+debugmode=false
+--permissions
+user_permission={
+  "INTERNET",
+  "SYSTEM_ALERT_WINDOW",
+  "WRITE_EXTERNAL_STORAGE",
+}
+--skip compilation
+skip_compilation={
+
+}
